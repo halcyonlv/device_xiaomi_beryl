af8699375341d015042a6fb93afc321eaead0039
Muhammad Fadly Saripudin <mdigreget@gmail.com>
2025-11-05 10:51:00 +0000

    beryl: Fix missing dependencies of mtk_agpsd

--- extract-files.py
+++ extract-files.py
@@ -154,8 +154,8 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so', 'vendor/bin/hw/android.hardware.gnss-service.mediatek'): blob_fixup()
     .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/bin/mtk_agpsd': blob_fixup()
-    .replace_needed('libcrypto.so', 'libcrypto-v32.so')
-    .add_needed('libssl-v32.so'),
+    .replace_needed('libcrypto.so', 'libcrypto-v33.so')
+    .add_needed('libssl.so'),
     'vendor/lib64/mt6855/libmnl.so': blob_fixup()
     .add_needed('libcutils.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
