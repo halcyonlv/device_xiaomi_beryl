d2aaec4ad6edc631c97884ed0955733817e84f21
Naoko Shoto <me@naokoshoto.my.id>
2026-08-18 07:20:44 +0200

    beryl: Welcome Halcyon Bloom (6.12 btw)
    Change-Id: Icbac223402a93a3c95409d20859297c367599b59

--- halcyon_beryl.mk
+++ halcyon_beryl.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit from beryl device
 $(call inherit-product, device/xiaomi/beryl/device.mk)
 
 PRODUCT_DEVICE := beryl
-PRODUCT_NAME := lineage_beryl
+PRODUCT_NAME := halcyon_beryl
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 14 5G
 PRODUCT_MANUFACTURER := Xiaomi
