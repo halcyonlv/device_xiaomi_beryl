daa6c7cbbebeba3da54c4ccd636e499adc3a5016
Naoko Shoto <me@naokoshoto.my.id>
2026-08-25 18:43:49 +0200

    beryl: dolby: Fix missing layout padding import in BandGainSlider

--- dolby/src/co/aospa/dolby/xiaomi/geq/ui/BandGainSlider.kt
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/BandGainSlider.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,9 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
-
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Surface
 
 @Composable
 fun BandGainSlider(
